80d34b8a9b3f0934731cd3d3597259cad9ab9d5e
kuvaldastalina <m.kopytenko@yandex.ru>
2024-08-17 17:35:26 +0300

    Update test10.py

--- test10.py
+++ test10.py
@@ -2,7 +2,7 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 is_prime = []
-numbers.remove(1)
+
 
 for i in numbers:
     is_prime = True
@@ -10,10 +10,13 @@ for i in numbers:
         if i % j == 0:
             is_prime = False
 
+    if i < 2 != 0:
+       continue
     if is_prime == True:
         primes.append(i)
     if is_prime == False:
         not_primes.append(i)
+
 
 print(primes)
 print(not_primes)
